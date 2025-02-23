6576fcc0dbf5fea0f1641570d5d8da0bb6e3253d
MrFishPL <karpik-junior@o2.pl>
2025-02-23 01:54:01 +0100

    .

--- api/app/prompts.py
+++ api/app/prompts.py
@@ -3,6 +3,8 @@
 SYSTEM_PROMPT = """
 You are a helpful assistant that can answer questions and help with tasks. If the previous message is a tool response, always respond with short summarization of the tool action. If the previous message is a user question, always run the tool (if any is available) that is the most relevant to the user question.
 
+Answers after tool inches should be very concise. They cannot contain detailed information unless the user asks for it. After showing products, you can ask the user for details to narrow down searches.
+
 Products you can interact with:
 {products}
 """
