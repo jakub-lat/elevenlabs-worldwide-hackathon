7c106aeeaae422c19824a5b41fc908d44f3d654a
Franekcode <mirkowskifranciszek@gmail.com>
2025-02-23 04:32:38 +0100

    go back to older start

--- frontend/src/routes/Start.tsx
+++ frontend/src/routes/Start.tsx
@@ -3,23 +3,18 @@ import SpeechIcon from "@/components/SpeechIcon";
 
 export default function Start() {
     return (
-        <div className="w-full h-full bg-[url(/images/7.jpg)] bg-cover bg-center">
-            <div className="flex flex-col justify-center items-center min-h-[100vh] w-full backdrop-blur-sm backdrop-saturate-50 bg-white/30">
-                <h1 className="text-center text-6xl font-bold mb-16 font-display tracking-tight">
-                    What do you want to buy today?
-                </h1>
-                <div className="scale-150 mb-8">
-                    <SpeechIcon />
-                </div>
-                <span className="text-sm text-gray-800 mt-4 tracking-wide uppercase">
+        <div className="w-full h-full bg-[url(/images/7.jpg)] bg-cover bg-center ">
+            <div className="flex flex-col justify-center items-center min-h-[100vh] w-full backdrop-blur-sm backdrop-saturate-50">
+                <h1 className="text-center text-3xl font-bold mb-12 font-display">What do you want to buy today?</h1>
+                <SpeechIcon />
+                <span className="text-xs text-gray-800 mt-4">
                     Hold Space to talk
                 </span>
-
-                <div className="absolute left-0 right-0 bottom-0 pb-32 w-full">
-                    <div className="mt-10 flex flex-col items-center">
-                        <span className="text-sm uppercase tracking-widest mb-4 text-gray-700">Inspirations</span>
-                        <Badges badges={["Winter jackets", "Tents", "Hiking boots", "Camping gear"]} />
-                    </div>
+            </div>
+            <div className="absolute left-0 right-0 bottom-0 pb-24 w-full">
+                <div className="mt-10 flex flex-col items-center">
+                    <span className="text-sm">Inspirations:</span>
+                    <Badges badges={["Winter jackets", "Tents", "Hiking boots", "Camping gear"]} />
                 </div>
             </div>
         </div>
