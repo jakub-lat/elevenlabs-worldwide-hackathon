e5e45958109c46c40847baaec5ad28a849753d9c
btrojan-official <bartosztrojanofficial@gmail.com>
2025-02-23 06:07:04 +0100

    moved SpeechBar a bit lower

--- frontend/src/components/SpeechBar.tsx
+++ frontend/src/components/SpeechBar.tsx
@@ -7,8 +7,8 @@ import { useEffect } from "react";
 export default function SpeechBar() {
     const [state, setState] = useAtom(bottomBarAtom);
         
-    return <div className="fixed bottom-0 left-0 right-0 py-8 flex justify-center z-[1000]">
-        <div className="py-4 px-24 flex flex-col items-center bg-white rounded-2xl shadow-lg z-[1000]">
+    return <div className="fixed bottom-[-20px] left-0 right-0 py-8 flex justify-center z-[1000]">
+        <div className="mt-10 py-2 bg-white shadow-lg px-20 flex flex-col items-center rounded-2xl z-[1000]">
             <div className="text-sm font-semibold text-gray-800">{state.text}</div>
             <div className={`${state.examples?.length && 'mb-4'} mt-2 flex flex-col items-center transition-all`}>
                 <Badges badges={state.examples} />
