95cf284fbfb06b31624593579b91f9ebef10cfc5
Franekcode <mirkowskifranciszek@gmail.com>
2025-02-23 05:12:54 +0100

    landing changes and databse json

--- frontend/src/routes/Start.tsx
+++ frontend/src/routes/Start.tsx
@@ -4,20 +4,17 @@ import { Link } from "react-router";
 
 export default function Start() {
     return (
-        <div className="w-full h-full bg-[url(/images/7.jpg)] bg-cover bg-center ">
+        <div className="w-full h-full bg-[url(/images/7.jpg)] bg-cover bg-center">
             <div className="flex flex-col justify-center items-center min-h-[100vh] w-full backdrop-blur-sm backdrop-saturate-50">
-                <h1 className="text-center text-3xl font-bold mb-12 font-display">What do you want to buy today?</h1>
+                <h1 className="text-center text-5xl font-bold mb-16 font-display">
+                    What do you want to buy today?
+                </h1>
                 <SpeechIcon />
-                <span className="text-xs text-gray-800 mt-4">
+                <span className="text-sm text-gray-800 mt-6">
                     <Link to="/browse">Hold Space to talk</Link>
                 </span>
             </div>
-            <div className="absolute left-0 right-0 bottom-0 pb-24 w-full">
-                <div className="mt-10 flex flex-col items-center">
-                    <span className="text-xs text-gray-800 mb-3">Get inspired:</span>
-                    <Badges badges={["Winter jackets", "Durable tents", "Hiking boots"]} />
-                </div>
-            </div>
+            
         </div>
     );
 }
